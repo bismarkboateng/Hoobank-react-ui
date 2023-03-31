64a51c2e66f581570f6c86f0f63cb81766336f9a
bismarkboateng <bismarkb609@gmail.com>
2023-03-31 06:41:38 +0000

    stat section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { robot, arrowUp, discount } from '../assets';
+import styles from '../style';
+
 
 const Hero = () => {
     return (
@@ -48,12 +50,12 @@ const Hero = () => {
                 </p>
             </div>
 
-            <div>
-                <div className='b-blue-gradient relative z-[0]'/>
-                <div className='white__gradient relative z-[1]'/>
-                <div className='pink__gradient z-[2]'/>
+            <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
 
-                <img src={robot} alt="robot image" className='mt-[3px] w-[500px] md:w-[800px]'/>
+                <img src={robot} alt="robot" className='w-[100%] h-[100%] relative z-[5]'/>
+                <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+                <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient' />
+                <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
             </div>
 
         </div>
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { stats } from '../constants/index';
+import styles from '../style';
 
 const Stats = () => {
   return (
-    <div>
-      Stats Section
+    <div className='flex flex-row mt-10
+        items-center justify-between font-poppins font-normal'>
+      {
+        stats.map( (stat) => (
+          <div key={stat.id} className='flex justify-start font-poppins font-normal items-center'>
+            <h1 className={`${styles.heading2}`}>{ stat.value }</h1>
+            <p className={`${styles.paragraph} ml-0 text-gradient`}>{stat.title}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
