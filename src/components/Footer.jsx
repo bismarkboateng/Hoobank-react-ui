84926b562fa9e66657606fe4b71284ebd689106a
bismarkboateng <bismarkb609@gmail.com>
2023-04-01 06:44:38 +0000

    footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,54 +1,66 @@
 import React from 'react';
 import { footerLinks } from '../constants';
-import { logo } from '../assets';
+import { logo, twitter, facebook, instagram, linkedin } from '../assets';
 import styles from '../style';
 
 const Footer = () => {
-  return (
-    <section className='
-        border border-red-500 mt-20 flex flex-row
-        font-poppins font-normal text-white
-        '>
 
-      <div>
-        <img src={logo} alt='site logo' className='mb-3'/>
+  footerLinks.map( (footerLink) => {
+      console.log(footerLink.title)
+  })
+
+  return (
+    <>
+    <section className='flex flex-row justify-between mt-20 pt-[20px]'>
+      
+      <div className='pt-2'>
+        <img src={logo} alt='logo image' />
         <p className={`${styles.paragraph}`}>
-          A new way to make the payments <br />
-          easy, reliable and secure
+          A new way to make the payments easy, <br />
+          reliable and secure
         </p>
       </div>
 
-
-
-      <div>
-      {
-        footerLinks.map((links) => {
-          <div key={links.key} className='font-poppins font-normal'>
-            <h5 className='font-poppins font-normal'>
-              { console.log(links.title)}
-               { links.title} 
-            </h5>
-
-            <ul>
+        {
+          footerLinks.map((footerLink) => (
+            <div className='text-white font-poppins font-normal'>
+              <ul>
+                <li className='font-semibold mb-2'> { footerLink.title} </li>
                 {
-                links.links.map( (link, index) => {
-                  <li key={link.name} className={`${styles.paragraph} 
-                    ${index !== link.length-1 ? 'mb-3' : 'mb-0'}`
-                  }>
-                    {console.log(link.name)}
-                    {link.name}
-                  </li>
-                })
+                  footerLink.links.map((link, index) => (
+                    <li className={`${styles.paragraph}
+                        ${index !== footerLink.length -1 ? 'mb-3' : 'mb-0'}
+                        cursor-pointer hover:text-blue`}> 
+                     {link.name} </li>
+                  ))
                 }
-            </ul>
+              </ul>
+            </div>
+          ))
+        }
+    </section>
 
-            
+    <section>
+          <div className='mt-10 w-[100%] bg-white h-[0.09px]' />
+
+          <div className='flex justify-between'>
+            <p className={`${styles.paragraph} mt-5`}>
+            2021 HooBank. All Right Reserved.
+            </p>
+
+            <div className='flex flex-row'>
+              <img src={instagram} alt='instagram logo' className='w-[20px] mr-5' />
+              <img src={facebook} alt='facebook logo' className='w-[20px] mr-5' />
+              <img src={twitter} alt='twitter logo' className='w-[20px] mr-5' />
+              <img src={linkedin} alt='linkedin logo' className='w-[20px]' />
+            </div>
           </div>
-        })
-      }
-      </div>
+
+
 
     </section>
+    </>
+
   )
 }
 
